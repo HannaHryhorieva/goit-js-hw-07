87a81ead6c534a87b351dc7cfe56d4d2823f5c73
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-04-27 20:43:42 +0300

    08-task ready

--- js/task-08.js
+++ js/task-08.js
@@ -2,37 +2,28 @@ const valueInput = document.querySelector('#controls input');
 const btnRender = document.querySelector('[data-action="render"]');
 const btnDestroy = document.querySelector('[data-action="destroy"]');
 const divParent = document.querySelector('#boxes');
-let amount = 0;
-const onValueInput = function (event) {
-    amount = Number(event.currentTarget.value);
-    // console.log(amount);
-    return amount
-};
-valueInput.addEventListener('input', onValueInput);
-// console.log(amount);
-let color = '';
 const makeColor = function () {
         const r = Math.floor(Math.random() * 255);
         const g = Math.floor(Math.random() * 255);
         const b = Math.floor(Math.random() * 255);
-        color = `(${r}, ${g}, ${b})`;
-        return color;
-    };
-    console.log(makeColor());
-const createBoxes = function (num) {
+        return `rgb(${r}, ${g}, ${b})`
+};
+   
+const createBoxes = function () {
+    const num = Number(valueInput.value);
     const arrayDivMarkup = [];
+    divParent.setAttribute('class', "div-collection")
     for (let i = 0; i < num; i += 1){
         const sizeDiv = 30 + i * 10;
-        const divMarkup = `<div class="div-collection" 
+        const divMarkup = `<div 
        style="width: ${sizeDiv}px; height: ${sizeDiv}px; 
-       background-color: ${makeColor};"></div>`;
+       background-color: ${makeColor()};"></div>`;
         arrayDivMarkup.push(divMarkup);   
     };
     return arrayDivMarkup.join('')
 };
-console.log(createBoxes(5));
 const onDivGallery = function () {
-    divParent.insertAdjacentHTML('afterbegin', createBoxes(amount))
+    return divParent.insertAdjacentHTML('afterbegin', createBoxes())
 };
 const destroyBoxes = function () {
     divParent.innerHTML = '';
